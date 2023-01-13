b637aa430e2b7ea25c49267787843c15b3b433d4
ThessaM <88784673+ThessaM@users.noreply.github.com>
2023-01-13 12:42:05 +0700

    update index js

--- src/index.js
+++ src/index.js
@@ -11,19 +11,116 @@ async function load_model() {
     // It's possible to load the model locally or from a repo
     // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
     //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
-    const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json");
+    const model = await loadGraphModel("https://github.com/ThessaM/Ai_Project_Object_Detection_SIBI/blob/main/models/web_model/model.json");
     return model;
   }
 
 let classesDir = {
-    1: {
-        name: 'Kangaroo',
-        id: 1,
-    },
-    2: {
-        name: 'Other',
-        id: 2,
+   1: { 
+        name:'A'
+        id:1
     }
+   2: { 
+        name:'B'
+        id:2
+    }
+   3: { 
+        name:'C'
+        id:3
+    }
+    4: { 
+        name:'D'
+        id:4
+    }
+    5: { 
+        name:'E'
+        id:5
+    }
+    6: { 
+        name:'F'
+        id:6
+    }
+    7: { 
+        name:'G'
+        id:7
+    }
+    8: { 
+        name:'H'
+        id:8
+    }
+    9: { 
+        name:'I'
+        id:9
+    }
+    10: { 
+        name:'J'
+        id:10
+    }
+    11: { 
+        name:'K'
+        id:11
+    }
+    12: { 
+        name:'L'
+        id:12
+    }
+    13: { 
+        name:'M'
+        id:13
+    }
+    14: { 
+        name:'N'
+        id:14
+    }
+    15: { 
+        name:'O'
+        id:15
+    }
+    16: { 
+        name:'P'
+        id:16
+    }
+    17: { 
+        name:'Q'
+        id:17
+    }
+    18: { 
+        name:'R'
+        id:18
+    }
+    19: { 
+        name:'S'
+        id:19
+    }
+    20: { 
+        name:'T'
+        id:20
+    }
+    21: { 
+        name:'U'
+        id:21
+    }
+    22: { 
+        name:'V'
+        id:22
+    }
+    23: { 
+        name:'W'
+        id:23
+    }
+    24: { 
+        name:'X'
+        id:24
+    }
+    25: { 
+        name:'Y'
+        id:25
+    }
+    26: { 
+        name:'Z'
+        id:26
+    }
+
 }
 
 class App extends React.Component {
